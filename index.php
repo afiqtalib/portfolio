a78d034735633bd1cf4653e7841a608189a05a6a
Muhammad Afiq <afiqtalib2580@gmail.com>
2022-08-15 22:59:06 +0800

    update

--- index.php
+++ index.php
@@ -21,8 +21,9 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h2>I am Muhammad Afiq</h2>
-                <h4>Looking for Internship Placement</h4>
+                <h2>I am Muhammad Afiq</h2><br>
+                <h4>Looking for Internship Placement</h4><br><br>
+                <h4>Web Developer • Web Designer • Data Analyst • Data Management • IT Support</h4><br><br>
             <button type="button" class="btn btn-outline-dark">See more ...</button>
             </div>
             <div class="col">
@@ -46,40 +47,43 @@
                 </div>
             </div>
             <div class="col-7">
-                <div class="p-3 border bg-light">
+                <div class="p-3 no-border">
                     <div class="row p-2">
                         <div class="col-1"><i class="bi bi-person-circle" style="font-size:2rem"></i></div>
                         <div class="col text">Muhammad Afiq Bin Talib Ali</div>
                     </div>
                     <div class="row p-2">
-                        <div class="col-3"><i class="bi bi-gender-male" style="font-size:2rem;  color: black;" ></i></div>
-                        <div class="col-6">22 Years Old</div>
+                        <div class="col-1"><i class="bi bi-gender-male" style="font-size:2rem;  color: black;" ></i></div>
+                        <div class="col-6 text">22 Years Old</div>
                     </div>
                     <div class="row p-2">
-                        <div class="col-3"><i class="bi bi-telephone" style="font-size:2rem;" ></i></div>
-                        <div class="col-6">011-21828562</div>
+                        <div class="col-1"><i class="bi bi-telephone" style="font-size:2rem;" ></i></div>
+                        <div class="col-6 text">011-21828562</div>
                     </div>
                     <div class="row p-2">
-                        <div class="col-3"><i class="bi bi-envelope" style="font-size:2rem;" ></i></div>
-                        <div class="col-6">afiqtalib2580@gmail.com</div>
+                        <div class="col-1"><i class="bi bi-envelope" style="font-size:2rem;" ></i></div>
+                        <div class="col-6 text">afiqtalib2580@gmail.com</div>
                     </div>
                     <div class="row p-2">
-                        <div class="col-3"><i class="fa fa-home" style="font-size:2rem;"></i></div>
-                        <div class="col-6">Kota Bharu, Kelantan</div>
+                        <div class="col-1"><i class="fa fa-home" style="font-size:2rem;"></i></div>
+                        <div class="col-6 text">Kota Bharu, Kelantan</div>
                     </div>
                     <div class="row p-2">
-                        <div class="col-3"><i class="fa fa-linkedin" style="font-size:2rem;"></i></div>
-                        <div class="col-6">linkedin.com/in/afiqtalib</div>
+                        <div class="col-1"><i class="fa fa-linkedin" style="font-size:2rem;"></i></div>
+                        <div class="col-6 text">linkedin.com/in/afiqtalib</div>
                     </div>
                 </div>
+                <div class="text text-center">
                     <span>My name is Muhammad Afiq Bin Talib Ali <br> I interested in web development 
                     I is hardworking and passionate person in venturing my interested and new things. 
                     My interested is in web desinger, web development and data management.
                     Currently, i looking for job related with my interested.describe about me
                     </span>
-                        <h5>talk about profile summary and email full name no contact</h5>
-                            <button type="button" class="btn btn-primary button">Download CV</button>
-
+                </div>
+                <br>
+                <div class="button">
+                    <button type="button" class="btn btn-light">Download CV</button>
+                </div>
             </div>
         </div>
 
@@ -93,7 +97,7 @@
             <h2>EDUCATION</h2>
         </div>
 
-        <div class="card" style="width: 90%;">
+        <div class="card border-left-primary" style="width: 90%;">
             <div class="card-body">
                 <div class="row">
                     <div class="col-7">
@@ -159,14 +163,46 @@
         <div class="section_heading">
             <h2>SKILLS</h2>
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">HTML</li>
-            <li class="list-group-item">CSS</li>
-            <li class="list-group-item">BOOTSTRAP</li>
-            <li class="list-group-item">MYSQL</li>
-            <li class="list-group-item">PHP</li>
-            <li class="list-group-item">Power BI</li>
-        </ul>
+        <div class="card shadow mb-4" style="width: 90%">
+            <div class="card-body">
+                <h4 class="small font-weight-bold">HTML<span
+                        class="float-right">90%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 90%"
+                        aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">CSS<span
+                        class="float-right">50%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 50%"
+                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">BOOTSTRAP<span
+                        class="float-right">75%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar" role="progressbar" style="width: 75%"
+                        aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">MYSQL<span
+                        class="float-right">50%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
+                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">PHP <span
+                        class="float-right">40%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 40%"
+                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">Power BI <span
+                        class="float-right">40%</span></h4>
+                <div class="progress">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 40%"
+                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
@@ -176,11 +212,35 @@
         <div class="section_heading">
             <h2>LANGUAGE</h2>
         </div>
-        <ul class="list-group list-group-flush">
+        <div class="card shadow mb-4" style="width: 90%; justify-content: center;">
+            <div class="card-body">
+                <h4 class="small font-weight-bold">MALAY<span
+                        class="float-right">90%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 90%"
+                        aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">ENGLISH<span
+                        class="float-right">65%</span></h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 65%"
+                        aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <h4 class="small font-weight-bold">ARABIC<span
+                        class="float-right">50%</span></h4>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 50%"
+                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                
+                
+            </div>
+        </div>
+        <!-- <ul class="list-group list-group-flush">
             <li class="list-group-item">MALAY</li>
             <li class="list-group-item">ENGLISH</li>
             <li class="list-group-item">ARABIC</li>
-        </ul>
+        </ul> -->
     </div>
 </section>
 
